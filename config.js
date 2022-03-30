0724fbe11c920ac8d7d289426060b40313fb9b54
girraj82 <96737336+girraj82@users.noreply.github.com>
2022-03-30 15:15:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,14 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
+  apiKey: 'AIzaSyDvL69RsECxOpvzGu5C6ZSoJVO4mQkTJQI',
+  authDomain: 'team-voting-app-ed0bb.firebaseapp.com',
+  databaseURL: 'https://team-voting-app-ed0bb-default-rtdb.firebaseio.com',
+  projectId: 'team-voting-app-ed0bb',
+  storageBucket: 'team-voting-app-ed0bb.appspot.com',
+  messagingSenderId: '447933454624',
+  appId: '1:447933454624:web:0d9afa31503c41b8b7df45',
+  measurementId: 'G-KTKHCPN3PL',
 
 };
 // Initialize Firebase
